e73b3ed544cad4484ea13eec53f99a033b19ba97
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-17 17:06:21 +0100

    route change, add filter_index method and a form in index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:index, :show] do
-    member do
-      get :crime
-    end
-  end
+  resources :users, only: [:index, :show]
   resources :orders, only: [:index]
   resources :crimes, only: [:index, :new, :create] do
     resources :orders, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  post "filter_index", to: "users#filter_index"
 end
