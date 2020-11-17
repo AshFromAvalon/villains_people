3a14220c1f591dcbf2208d9edcd046852cd20f54
Maximin Daudiffret <maximin.daudiffret@gmail.com>
2020-11-17 14:08:27 +0100

    order controller, new, create and index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:index, :show]
-  resources :crimes, only: [:index, :new, :create]
+  resources :orders, only: [:index]
+  resources :crimes, only: [:index, :new, :create] do
+    resources :orders, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index]
+
 end
