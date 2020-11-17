96957b7b3835bf1d171ee748ad3c38b45aa3a769
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-17 18:42:21 +0100

    filter crimes thanks to checkboxes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,26 @@ class UsersController < ApplicationController
   end
 
   def index
+    #if the user has filtered
     if params[:query]
-      @users =  User.includes(:crimes).where('crimes.category' => params[:query])
+      #params[:query] is an array of crimes selected via checkboxes
+      params[:query].each_with_index do |crime, index|
+        if index == 0
+          #to instantiate an active record collection
+          @users = User.includes(:crimes).where('crimes.category' => crime)
+        else
+          #adds other crimes selected to the collection
+          @users.merge(User.includes(:crimes).where('crimes.category' => crime))
+        end
+      end
     else
       @users =  User.includes(:crimes).where.not('crimes.id' => nil)
     end
   end
 
   def filter_index
-    redirect_to action: "index", query: params[:query]
+    #isolate params that are in our crimes list
+    selected_crimes = params.keys.select { |crime| Crime::CATEGORIES.include?(crime) }
+    redirect_to action: "index", query: selected_crimes
   end
 end
