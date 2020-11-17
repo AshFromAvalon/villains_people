ce27c64a9978180e7f122a2247709aeca4035499
Maximin Daudiffret <maximin.daudiffret@gmail.com>
2020-11-17 12:37:45 +0100

    reverted PagesCOntroller to previous state that was ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :users]
+  skip_before_action :authenticate_user!, only: [:home]
   def home
     redirect_to users_path
   end
