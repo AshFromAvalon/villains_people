4e8a593f85556585f05f7d25e43346a92297857b
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-17 15:30:36 +0100

    add crime_user_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do
+      get :crimes
+    end
+  end
   resources :orders, only: [:index]
   resources :crimes, only: [:index, :new, :create] do
     resources :orders, only: [:new, :create]
