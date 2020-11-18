fabef758ba476dcc84c2d973d3e608717dcabc72
Thomas Le Bihan <thomas.66days@gmail.com>
2020-11-18 21:04:15 +0100

    added title + review button on orders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @reviews = Review.all.select do |review|
       review.order.user_id == params[:id].to_i
     end
+    @ratings = Review.all.map { |review| review.rating }
+    @average_rating = @ratings.sum.fdiv(@ratings.size)
   end
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_171850) do
+ActiveRecord::Schema.define(version: 2020_11_18_194753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_171850) do
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["order_id"], name: "index_reviews_on_order_id"
   end
 
