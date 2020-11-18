e82a651372c7c6c671e16698309d9f387811df36
Maximin Daudiffret <maximin.daudiffret@gmail.com>
2020-11-18 13:57:27 +0100

    cancel order & group of orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_112632) do
+ActiveRecord::Schema.define(version: 2020_11_18_110006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 2020_11_17_112632) do
     t.bigint "crime_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "request_date"
+    t.date "done_date"
+    t.boolean "cancelled"
     t.index ["crime_id"], name: "index_orders_on_crime_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
