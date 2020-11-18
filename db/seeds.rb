72dbe90f595615a96cb5217f564bd2698894dfe8
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-18 16:00:34 +0100

    seed for vilains and regular user + crime+ order

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,36 @@
 require 'open-uri'
 #users
 
-puts"create users"
 
 Order.destroy_all if Rails.env.development?
 Crime.destroy_all if Rails.env.development?
 User.destroy_all if Rails.env.development?
 
+puts"create villains"
 
+#add url pic
 dalton_pic = URI.open('https://i.ytimg.com/vi/u2xWnpl_Rzg/maxresdefault.jpg')
 
-dalton = User.new(email: "daltons@daltons.con", password:"123456", user_name: "The Daltons", alias: "The Daltons", description: "Best thieves in town")
+dalton = User.new(email: "daltons@daltons.con", password:"123456", user_name: "The Daltons", alias: "The Daltons", description: "Best thieves in town", rating: 3)
 dalton.photo.attach(io: dalton_pic, filename: 'daltons.jpg', content_type: 'image/jpg')
-dalton.save
+dalton.save!
 
+#Add your villain by change params of User.new bellow
+# YOUR_VILLAIN = User.new(email: "YOUR_VILLAIN@YOUR_VILLAIN.con", password:"123456", user_name: "YOUR_VILLAIN USER NAME", alias: "YOUR_VILLAIN ALIAS", description: "YOUR_VILLAIN DESCRIPTION")
+# YOUR_VILLAIN.photo.attach(io: YOUR_VILLAIN_pic, filename: 'YOUR_VILLAINs.jpg', content_type: 'image/jpg')
+# YOUR_VILLAIN.save
 
-puts"create crimes"
+puts "create regular users"
 
-dalton_crime = Crime.create!(user_id: dalton.id, name: "bank robbery", category: "Thief", price: "10", currency: "dogs" )
+max = User.new(email: "max@max.con", password:"123456", user_name: "Amazing Max")
+max.save
+
+puts"create villain crimes"
+
+dalton_crime = Crime.create!(user_id: dalton.id, name: "bank robbery", category: "Thief", price: 10, currency: "dogs" )
+#Add your villain crimes by changing params bellow
+# YOUR_VILLAIN_crime = Crime.create!(user_id: YOUR_VILLAIN.id, name: "bank robbery", category: "Thief", price: "10", currency: "dogs" )
+
+puts "create regular users orders"
+
+max_order = Order.new(user_id: max.id, crime_id: dalton_crime.id, description: "Please help me to steal my sister's toys !")
