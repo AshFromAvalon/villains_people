0fca73ba2ae8be1f7949b746ccd8863060f5d4c5
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-18 14:56:32 +0100

    seed file with Daltons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-x = 1
+# x = 1
 
-10.times do
-  name = "michel" + x.to_s
-  x += 1
-  email = name + "@michel.com"
-  User.create!(user_name: name, email: email, password: "azerty")
-end
+# 10.times do
+#   name = "michel" + x.to_s
+#   x += 1
+#   email = name + "@michel.com"
+#   User.create!(user_name: name, email: email, password: "azerty")
+# end
+require 'open-uri'
+#users
+
+puts"create users"
+
+Order.destroy_all if Rails.env.development?
+Crime.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
+
+
+dalton_pic = URI.open('https://i.ytimg.com/vi/u2xWnpl_Rzg/maxresdefault.jpg')
+
+dalton = User.new(email: "daltons@daltons.con", password:"123456", user_name: "The Daltons", alias: "The Daltons", description: "Best thieves in town")
+dalton.photo.attach(io: dalton_pic, filename: 'daltons.jpg', content_type: 'image/jpg')
+dalton.save
+
+
+puts"create crimes"
+
+dalton_crime = Crime.create!(user_id: dalton.id, name: "bank robbery", category: "Thief", price: "10", currency: "dogs" )
