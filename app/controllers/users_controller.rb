66c7470afa0ddb8f428836e72ea5d626d2a799b5
Maximin Daudiffret <maximin.daudiffret@gmail.com>
2020-11-20 15:41:28 +0100

    correction on review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = Review.all.select do |review|
-      review.order.user_id == params[:id].to_i
+      review.order.crime.user_id == params[:id].to_i
     end
     @ratings = @reviews.map { |review| review.rating }
     @average_rating = @ratings.sum.fdiv(@ratings.size)
